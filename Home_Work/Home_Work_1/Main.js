01689ff92c11569ff0ca413ec12fe03ad7d9e59d
Bubusyn <dvport71@ukr.net>
2022-07-24 23:30:56 +0300

    js

---
+++ Home_Work/Home_Work_1/Main.js
@@ -0,0 +1,94 @@
+// let str1 = 'Hello ';
+// let str2 = "owu.";
+// let str3 = "com.";
+// let str4 = "ua.";
+// console.log( str1+str2+str3+str4 );
+// alert(str1+str2+str3+str4 );
+// document.write( str1+str2+str3+str4  );
+//
+// let str5 = 1;
+// console.log( str5 );
+// alert(str5);
+// document.write( " 1, " );
+//
+// let str6 = 10;
+// console.log( str6 );
+// alert(str6);
+// document.write( " 10, " );
+//
+// let str7 = -999;
+// console.log( str7 );
+// alert(str7);
+// document.write( " -999, " );
+//
+// let str8 = 123;
+// console.log( str8 );
+// alert(str8);
+// document.write( ' 123 ' );
+//
+// let str9 = 3.14;
+// console.log( str9 );
+// alert(str9);
+// document.write( " 3.14, ");
+//
+// let str10 = 2.7;
+// console.log( str10 );
+// alert(str10);
+// document.write( ' 2.7, ' );
+//
+// let str11 = 16;
+// console.log( str11 );
+// alert(str11);
+// document.write( ' 16 ' );
+//
+// let bool1 = true;
+// console.log( bool1 );
+// alert ( bool1 );
+// document.write( ' true ' );
+//
+// let bool2 = false;
+// console.log( bool2 );
+// alert(bool2);
+// document.write( " falshe");
+//
+
+let firstName = 'Дмитро ';
+let middleName = ' Валерійович ';
+let lastName = ' Портянніков';
+let person = firstName + middleName + lastName;
+console.log(person);
+document.write(person);
+
+
+// console.log('112233');
+// let a; // undefined
+// a = 100;
+// console.log(a);
+// a = 100500;
+// console.log(a);
+//
+// let str1 = 'Hello';
+// let str2 = " Okten ";
+// let res = str1+str2;
+// console.log(res);
+// let resr = `${str1} ${str2}`;
+// console.log(resr);
+//
+// let s ='!!!';
+// let n1=10;
+// let n2=20;
+// console.log(n1+n2+s);
+// console.log(s+(n1+n2));
+
+// bool > < = == ! //
+
+// console.log(5>6);
+// console.log(100<200);
+// console.log(100 !== 200);
+// console.log('asd' == 'asd');
+// console.log(0 == '0');
+// console.log(0 === '0');
+// console.log(typeof 0 === typeof '0');
+
+// let inputData = prompt('name','dat');
+// console.log(!!inputData);
