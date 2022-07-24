8dfd07ecd1d05f7a5255990e4d1bafa29d232fef
Bubusyn <dvport71@ukr.net>
2022-07-25 00:40:04 +0300

    js1

--- Home_Work/Home_Work_1/HW_6/Main.js
+++ Home_Work/Home_Work_1/HW_6/Main.js
@@ -1,4 +1,4 @@
 let inputData1 = prompt('firstName');
 let inputData2 = prompt('middleName');
-let inputData3 = prompt('data');
+let inputData3 = prompt('You age ?');
 console.log( inputData1 + inputData2 + inputData3);
