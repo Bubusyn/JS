ef0032905384993ee6905ea348f4af8e70549908
Bubusyn <dvport71@ukr.net>
2022-07-27 21:01:57 +0300

    js2 end

--- Home_Work/Home_Work_2/Main.js
+++ Home_Work/Home_Work_2/Main.js
@@ -47,36 +47,62 @@
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
 
-    let book1 = {
-
-        title: 'Вечера на хуторе близь Диканьки',
-        pageCount: 42,
-        genre: "Повести",
-        autor: [
-            {name: "Николай Гоголь", age: 45,}
-        ],
-    }
-
-let book2 = {
-    title: 'Руслан и Людмила',
-    pageCount: 62,
-    genre: "Поэма",
-    autor: [
-        {name: "Александр Пушкин", age: 32,}
-    ],
-}
-let book3 = {
-    title: 'Охота за "Красным октябрем"',
-    pageCount: 262,
-    genre: "Роман",
-    autor: [
-        {name: "Том Кленси", age: 65,}
-    ],
-}
-console.log(book1, book2, book3);
-
-
+//     let book1 = {
+//
+//         title: 'Вечера на хуторе близь Диканьки',
+//         pageCount: 42,
+//         genre: "Повести",
+//         autor: [
+//             {name: "Николай Гоголь", age: 43,}
+//         ],
+//     }
+//
+// let book2 = {
+//     title: 'Руслан и Людмила',
+//     pageCount: 62,
+//     genre: "Поэма",
+//     autor: [
+//         {name: "Александр Пушкин", age: 38,}
+//     ],
+// }
+// let book3 = {
+//     title: 'Охота за "Красным октябрем"',
+//     pageCount: 262,
+//     genre: "Роман",
+//     autor: [
+//         {name: "Том Кленси", age: 66,}
+//     ],
+// }
+// console.log(book1, book2, book3)
+//
+//
 
 
 
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let users = [
+    {name: "Дмитро", username: "Bubusyn", password: 'rr123' },
+    {name: "Петро", username: "Alex", password: '12r3y' },
+    {name: "Віктор", username: "Vav", password: 'fff12' },
+    {name: "Дмитро", username: "Susyn", password: '123gg' },
+    {name: "Даніла", username: "Bizy", password: '1fh23' },
+    {name: "Сергій", username: "Boss", password: '1rr23' },
+    {name: "Павлоо", username: "Bingo", password: '1gg23' },
+    {name: "Серго", username: "Braser", password: '12gg3' },
+    {name: "Армен", username: "Hippi", password: 'uu123' },
+    {name: "Яна", username: "Love", password: '123uu' },
+
+]
+console.log(users)
+console.log(users[0].name,users[0].password);
+console.log(users[1].name,users[1].password);
+console.log(users[2].name,users[2].password);
+console.log(users[3].name,users[3].password);
+console.log(users[4].name,users[4].password);
+console.log(users[5].name,users[5].password);
+console.log(users[6].name,users[6].password);
+console.log(users[7].name,users[7].password);
+console.log(users[8].name,users[8].password);
+console.log(users[9].name,users[9].password);
+
