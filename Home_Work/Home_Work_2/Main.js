3c2f6957ee550478453023ee88fc98e88cb1ae8d
Bubusyn <dvport71@ukr.net>
2022-07-27 11:30:52 +0300

    js2

---
+++ Home_Work/Home_Work_2/Main.js
@@ -0,0 +1,34 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+//
+// let arr;
+// arr = [123, 456, 789, -123, -456, -789, 0, true, 'Okten School', false, ];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+
+
+let book = {
+    title: 1,
+    pageCount: 'vaysa',
+    genre: 31
+}
+
+console.log(book);
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
