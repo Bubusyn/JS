573792f60227f334f267e636cc6fd15738e53601
Bubusyn <dvport71@ukr.net>
2022-08-01 00:02:51 +0300

    js3 end

--- Home_Work/Home_Work_1/HW_3/Main.js
+++
@@ -1,7 +0,0 @@
-
-let firstName = 'Дмитро ';
-let middleName = ' Валерійович ';
-let lastName = ' Портянніков';
-let person = firstName + middleName + lastName;
-console.log(person);
-document.write(person);
